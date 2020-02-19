05b504bfa1a91c7e3ebf7a5c7cb8c68f5b70a6d8
Bradley Reynolds <breynolds3@my.ancoraed.com>
2020-02-18 18:26:16 -0600

    Add project files.

---
+++ Lab2_5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Lab2_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 10;
+            int secondNumber = 5;
+            int thirdNumber = 2;
+
+            Console.WriteLine(firstNumber + secondNumber);
+            Console.WriteLine(firstNumber - secondNumber);
+            Console.WriteLine(firstNumber * secondNumber);
+            Console.WriteLine(firstNumber / secondNumber);
+
+            Console.WriteLine("Hello Bradley");
+
+            Console.WriteLine(firstNumber * secondNumber * thirdNumber);
+
+
+            Console.WriteLine("Enter your age");
+            string userAge = Console.ReadLine();
+            Console.WriteLine("Woah! You look way younger than " + userAge);
+
+
+           
+
+
+
+
+
+
+        }
+    }
+}
